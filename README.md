b2a21197f18a00221593b3b8a0e09cf17123b551
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-05 20:03:39 +0545

    Add db configuration

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Random Project
+##Random Project
 
 This is a random backend project. I might add some frontend later on if i feel like it.
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const port = process.env.PORT || 8080;
+
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
