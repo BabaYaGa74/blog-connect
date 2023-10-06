14ae30c86e36fc3726b3a1e312dfb8edce803758
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-06 11:53:43 +0545

    Add create and get

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,3 +1,17 @@
-export const model = () => {
-  console.log("Model connection!");
+const db = require("../config/dbConfig");
+
+const UserModel = {
+  getAll: (callback) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, [], callback);
+  },
+
+  create: (userdata, callback) => {
+    const { name, username, email, password } = userdata;
+    const qry =
+      "INSERT INTO users (name, username, email, password) VALUES (?,?,?,?)";
+    db.query(qry, [name, username, email, password], callback);
+  },
 };
+
+module.exports = UserModel;
