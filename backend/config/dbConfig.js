04880f673fd956a2557d16282c068fff69e60e94
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-06 09:04:25 +0545

    Add mysql connection

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,3 +1,13 @@
-export const database = () => {
-  console.log("Database connection");
-};
+const mysql = require("mysql2");
+
+const pool = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mydb",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
