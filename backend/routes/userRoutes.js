3d5fa8fee380a8f011c0a6d8a26158ace622b96c
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-09 14:35:35 +0545

    Update userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllUsers,
-  createUser,
   getUser,
   updateUser,
   deleteUser,
 } = require("../controllers/userController");
 
 router.get("/", getAllUsers);
-router.post("/user", createUser);
 router.route("/user/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
