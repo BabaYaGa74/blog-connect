20405686f2d1ba81d73c9b3f359a32149b4c0532
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-09 14:35:04 +0545

    Update UserModel.js

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -6,13 +6,6 @@ const UserModel = {
     db.query(sql, [], callback);
   },
 
-  create: (userdata, callback) => {
-    const { name, username, email, password } = userdata;
-    const qry =
-      "INSERT INTO users (name, username, email, password) VALUES (?,?,?,?)";
-    db.query(qry, [name, username, email, password], callback);
-  },
-
   getUserById: (id, callback) => {
     const qry = "SELECT * FROM users WHERE id = ?";
     db.query(qry, [id], callback);
