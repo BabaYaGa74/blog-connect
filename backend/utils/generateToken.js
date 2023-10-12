b3535e30a89abab9ede7cbeb1206dd94881b859b
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-12 18:29:41 +0545

    Token Generation

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,3 +1,12 @@
-const Token = () => {
-  console.log("Generate token here");
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = "jsonwebTokenKey";
+const generateToken = (id) => {
+  jwt.sign({ id }, jwtSecret, {}, (err, result) => {
+    if (err) throw new Error("Error while creating token");
+
+    console.log(result);
+  });
 };
+
+module.exports = generateToken;
