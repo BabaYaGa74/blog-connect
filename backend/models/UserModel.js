9ce13519ba8deb76d1fae0cf0047c771a84f9bc8
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-12 14:19:38 +0545

    Update UserModel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -8,19 +8,43 @@ const UserModel = {
 
   getUserById: (id, callback) => {
     const qry = "SELECT * FROM users WHERE id = ?";
-    db.query(qry, [id], callback);
+    db.query(qry, [id], (err, result) => {
+      if (err) {
+        return callback(err, []);
+      } else if (result.length == 1) {
+        return callback(null, result);
+      } else {
+        return callback(null, []);
+      }
+    });
   },
 
   update: (id, userData, callback) => {
     const { name, username, email, password } = userData;
     const qry =
       "UPDATE users SET name=?, username=?, email=?, password=? WHERE id = ?";
-    db.query(qry, [name, username, email, password, id], callback);
+    db.query(qry, [name, username, email, password, id], (err, result) => {
+      if (err) {
+        return callback(err, []);
+      } else if (result.length == 1) {
+        return callback(null, result);
+      } else {
+        return callback(null, []);
+      }
+    });
   },
 
   delete: (id, callback) => {
     const qry = "DELETE FROM users WHERE id =?";
-    db.query(qry, [id], callback);
+    db.query(qry, [id], (err, result) => {
+      if (err) {
+        return callback(err, []);
+      } else if (result.length == 1) {
+        return callback(null, result);
+      } else {
+        return callback(null, []);
+      }
+    });
   },
 };
 
