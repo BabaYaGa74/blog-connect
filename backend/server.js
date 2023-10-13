f4c4c33f8a3c8d734b75edac9383ccce880357f6
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-13 14:21:00 +0545

    Update server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
