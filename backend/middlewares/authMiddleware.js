4e6c1f31ed1b314103a2e29d88c35a1b34b27187
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-13 14:21:46 +0545

    Add protect funct

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,3 +1,20 @@
-export const auth = () => {
-  console.log("anything");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/userModel");
+
+export const protect = async (req, res, next) => {
+  let token = req.cookies.jwtToken;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await UserModel.getUserById(decoded.userId).select(
+        "-password"
+      );
+      next();
+    } catch (error) {
+      console.error("Error during token verification: ", error);
+      res.status(500).send("No authorization, Invalid token");
+    }
+  } else {
+    res.status(500).send("No authorization, No token");
+  }
 };
