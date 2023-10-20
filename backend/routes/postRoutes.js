55156b7878e81b852cc79034a0c324f8c38b6db1
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-20 13:04:55 +0545

    Update Routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { createPost } = require("../controllers/postController");
+const {
+  createPost,
+  getAllPosts,
+  getSinglePost,
+  updatePost,
+  deletePost,
+  getUserPost,
+} = require("../controllers/postController");
 
 router.post("/create", createPost);
+router.get("/all", getAllPosts);
+router.get("/post/:id", getSinglePost);
+router.put("/post/:id", updatePost);
+router.delete("/post/:id", deletePost);
+router.get("/post/user/:id", getUserPost);
 
 module.exports = router;
