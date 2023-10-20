27a9817d3936de721581b666d70d20643113197d
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-20 14:14:21 +0545

    Add comment model

---
+++ backend/models/CommentModel.js
@@ -0,0 +1,46 @@
+const db = require("../config/dbConfig");
+
+const CommentModel = {
+  create: (commentData) => {
+    return new Promise((resolve, reject) => {
+      const { content, username, userId, postId } = commentData;
+      const qry =
+        "INSERT INTO comments(content, username, userId, postId) VALUES (?,?,?,?)";
+      db.query(qry, [content, username, userId, postId], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  getAll: (id) => {
+    return new Promise((resolve, reject) => {
+      const qry = "SELECT * FROM comments WHERE postId = ?";
+      db.query(qry, [id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+  edit: (commentCon, id) => {
+    return new Promise((resolve, reject) => {
+      const qry = "UPDATE comments SET content =? WHERE commentId=?";
+      db.query(qry, [commentCon, id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  remove: (id) => {
+    return new Promise((resolve, reject) => {
+      const qry = "DELETE FROM comments WHERE commentId=?";
+      db.query(qry, [id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+};
+
+module.exports = CommentModel;
