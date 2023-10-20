0a5ddd6204d046d288b1dc15683d1a95af0cd061
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-20 13:04:32 +0545

    Update Model

--- backend/models/PostModel.js
+++ backend/models/PostModel.js
@@ -10,13 +10,62 @@ const PostModel = {
         qry,
         [title, description, username, userId, category],
         (err, result) => {
-          if (err) {
-            return reject(err);
-          } else {
-            return resolve(result);
-          }
+          if (err) return reject(err);
+          return resolve(result);
         }
       );
+    });
+  },
+
+  allPosts: () => {
+    return new Promise((resolve, reject) => {
+      const qry = "SELECT * FROM posts";
+      db.query(qry, [], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  singlePost: (id) => {
+    return new Promise((resolve, reject) => {
+      const qry = "SELECT * FROM posts WHERE postId = ?";
+      db.query(qry, [id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  updatePost: (userData, id) => {
+    const { title, description, category } = userData;
+    return new Promise((resolve, reject) => {
+      const qry =
+        "UPDATE posts SET title=?, description=?, category=? WHERE postId =?";
+      db.query(qry, [title, description, category, id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  delete: (id) => {
+    return new Promise((resolve, reject) => {
+      const qry = "DELETE FROM posts WHERE postId = ?";
+      db.query(qry, [id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
+    });
+  },
+
+  userPost: (userId) => {
+    return new Promise((resolve, reject) => {
+      const qry = "SELECT * FROM posts WHERE userId =?";
+      db.query(qry, [userId], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
     });
   },
 };
