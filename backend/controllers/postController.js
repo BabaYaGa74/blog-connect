8c5463556b532f586616912cf6fc321bfb929f09
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-10-20 13:04:07 +0545

    Update controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -18,6 +18,77 @@ const createPost = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await PostModel.allPosts();
+    console.log(posts);
+    res.status(200).send({ message: "Posts found!", posts });
+  } catch (error) {
+    console.error("Cannot fetch posts", error);
+    res.status(400).send("Error occured while fetching posts");
+  }
+};
+
+const getSinglePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await PostModel.singlePost(id);
+    console.log(post);
+    res.status(200).send({ message: "Post found!", post: post[0] });
+  } catch (error) {
+    console.error("Cannot fetch the post", error);
+    res.status(400).send("Error occured while fetching post");
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { title, description, category } = req.body;
+    const { id } = req.params;
+    const updatedPost = await PostModel.updatePost(
+      { title, description, category },
+      id
+    );
+    res
+      .status(200)
+      .send({ message: "Post updated Successfully!", updatedPost });
+  } catch (error) {
+    console.error("Cannot update the post", error);
+    res.status(400).send("Error! Cannot Update");
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedPost = await PostModel.delete(id);
+    console.log(deletedPost);
+    res.status(200).send({ Message: "Post Deleted Successfully", deletedPost });
+  } catch (error) {
+    console.error("Cannot delete the post", error);
+    res.status(400).send("Error! Cannot delete post");
+  }
+};
+
+const getUserPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userPosts = await PostModel.userPost(id);
+    console.log(userPosts);
+    res
+      .status(200)
+      .send({ message: "User's post fetched successfully!", userPosts });
+  } catch (error) {
+    console.error("Cannot fetch post", error);
+    res.status(400).send("Error occured while fetching user's post");
+  }
+};
+
 module.exports = {
   createPost,
+  getAllPosts,
+  getSinglePost,
+  updatePost,
+  deletePost,
+  getUserPost,
 };
