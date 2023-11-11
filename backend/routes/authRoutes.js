a036a77718feebd3731e5247c4cf08d4041125b4
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-11 09:40:52 +0545

    Update Auth Routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,11 +4,13 @@ const {
   registerUser,
   loginUser,
   reFetchUser,
+  logoutUser,
 } = require("../controllers/authController");
 const protect = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 router.get("/refetch", protect, reFetchUser);
 
 module.exports = router;
