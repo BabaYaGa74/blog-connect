5c41bc5492c5a400541d3d42c90bce8889dc9360
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-17 20:23:47 +0545

    Update Vote

--- backend/models/VoteModel.js
+++ backend/models/VoteModel.js
@@ -1,56 +1,31 @@
 const db = require("../config/dbConfig");
 
 const votemodel = {
-  checkVote: (userId, postId) => {
-    return new Promise((resolve, reject) => {
-      const qry = "SELECT * FROM vote WHERE userId = ? AND postId = ?";
-      db.query(qry, [userId, postId], (err, result) => {
-        if (err) return reject(err);
-        return resolve(result);
-      });
-    });
+  vote: async (postId, userId) => {
+    const query = "INSERT INTO vote (postId, userId) VALUES (?, ?)";
+    const values = [postId, userId];
+
+    try {
+      await db.query(query, values);
+      return { success: true, message: "Vote recorded successfully." };
+    } catch (error) {
+      console.error("Error voting:", error);
+      throw new Error("Error voting for the post.");
+    }
   },
 
-  allVotes: (postId) => {
-    return new Promise((resolve, reject) => {
-      const qry = "SELECT * FROM vote WHERE postId = ?";
-      db.query(qry, [postId], (err, result) => {
-        if (err) return reject(err);
+  // Function to unvote a post
+  unvote: async () => {
+    const query = "DELETE FROM votes WHERE post_id = ? AND user_id = ?";
+    const values = [this.postId, this.userId];
 
-        return resolve(result);
-      });
-    });
-  },
-
-  votePost: (postId, userId, voteType) => {
-    return new Promise((resolve, reject) => {
-      const qry = "INSERT INTO vote(postId, userId, voteType) VALUES (?,?,?)";
-      db.query(qry, [postId, userId, voteType], (err, result) => {
-        if (err) return reject(err);
-        return resolve(result);
-      });
-    });
-  },
-
-  updateVote: (voteType, userId, postId) => {
-    return new Promise((resolve, reject) => {
-      const qry =
-        "UPDATE vote SET voteType = ? where userId = ? AND postId = ?";
-      db.query(qry, [voteType, userId, postId], (err, result) => {
-        if (err) return reject(err);
-        return resolve(result);
-      });
-    });
-  },
-
-  removeVote: (userId, postId) => {
-    return new Promise((resolve, reject) => {
-      const qry = "DELETE FROM vote WHERE userId = ? AND postId = ?";
-      db.query(qry, [userId, postId], (err, result) => {
-        if (err) return reject(err);
-        return resolve(result);
-      });
-    });
+    try {
+      await db.query(query, values);
+      return { success: true, message: "Vote removed successfully." };
+    } catch (error) {
+      console.error("Error unvoting:", error);
+      throw new Error("Error removing vote for the post.");
+    }
   },
 };
 
