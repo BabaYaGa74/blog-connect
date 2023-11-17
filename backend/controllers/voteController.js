9e9288e7a2edbde65981cd859a80db7ede9d2f89
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-17 20:36:19 +0545

    Update Vote Controller

--- backend/controllers/voteController.js
+++ backend/controllers/voteController.js
@@ -14,41 +14,20 @@ const getVotes = async (req, res) => {
 const vote = async (req, res) => {
   try {
     const { userId, postId } = req.body;
-    const result = await VoteModel.checkVote(userId, postId);
+    const result = await VoteModel.vote(postId, userId);
     res.status(200).send({ success: "true", result: result });
-
-    // let voteType, vote;
-    // if (result.length === 0) {
-    //   voteType = "upvote";
-    //   vote = await VoteModel.votePost(postId, userId, voteType);
-    //   res.status(200).send({ success: "true", result: vote });
-    // } else if (result.length === 1 && result[0].voteType == "upvote") {
-    //   voteType = "downvote";
-    //   vote = await VoteModel.updateVote(voteType, userId, postId);
-    //   res.status(200).send({ success: "true", result: vote });
-    // } else if (result.length === 1 && result[0].voteType == "downvote") {
-    //   const voteType = "upvote";
-    //   vote = await VoteModel.updateVote(voteType, userId, postId);
-    //   res.status(200).send({ success: "true", result: vote });
-    // } else {
-    //   res
-    //     .status(500)
-    //     .send({ success: "false", ERROR: "Registered more than once!" });
-    // }
   } catch (error) {
     res.status(400).send("Unable to vote!!");
-    console.log("Failed to vote: ", error);
   }
 };
 
 const unVote = async (req, res) => {
   try {
     const { userId, postId } = req.body;
-    const result = await VoteModel.removeVote(userId, postId);
-    res.status(200).send({ success: "true", vote: result });
+    const result = await VoteModel.unvote(postId, userId);
+    res.status(200).send({ success: "true", unVote: result });
   } catch (error) {
     res.status(400).send("Unable to unvote!!");
-    console.log("Failed to unvote: ", error);
   }
 };
 
