d4eab0740c5f918098384d008286d6daa078d2e6
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-21 12:23:24 +0545

    Update vote Controller

--- backend/controllers/voteController.js
+++ backend/controllers/voteController.js
@@ -14,10 +14,18 @@ const getVotes = async (req, res) => {
 const vote = async (req, res) => {
   try {
     const { userId, postId } = req.body;
-    const result = await VoteModel.vote(postId, userId);
-    const voteCount = await VoteModel.getVoteCount(postId);
-    console.log("Votes:", voteCount);
-    res.status(200).send({ success: "true", result: result, voteCount });
+    const checkResult = await VoteModel.checkUser(postId, userId);
+    let voteCount = 0;
+    if (checkResult === false || checkResult !== "upvote") {
+      if (checkResult === "downvote") {
+        await VoteModel.deleteVote(postId, userId, "downvote");
+      }
+      const result = await VoteModel.vote(postId, userId);
+      voteCount = await VoteModel.getVoteCount(postId);
+      return res.status(200).send({ success: true, result: result, voteCount });
+    } else {
+      return res.status(200).send({ success: false });
+    }
   } catch (error) {
     res.status(400).send("Unable to vote!!");
   }
@@ -26,9 +34,19 @@ const vote = async (req, res) => {
 const downVote = async (req, res) => {
   try {
     const { userId, postId } = req.body;
-    const result = await VoteModel.downvote(postId, userId);
-    const voteCount = await VoteModel.getVoteCount(postId);
-    res.status(200).send({ success: "true", unVote: result, voteCount });
+    const checkResult = await VoteModel.checkUser(postId, userId);
+    let voteCount = 0;
+    if (checkResult === false || checkResult !== "downvote") {
+      if (checkResult === "upvote") {
+        const result = await VoteModel.deleteVote(postId, userId, "upvote");
+        console.log(result);
+      }
+      const result = await VoteModel.downvote(postId, userId);
+      voteCount = await VoteModel.getVoteCount(postId);
+      return res.status(200).send({ success: true, unVote: result, voteCount });
+    } else {
+      return res.status(200).send({ success: false });
+    }
   } catch (error) {
     res.status(400).send("Unable to unvote!!");
   }
