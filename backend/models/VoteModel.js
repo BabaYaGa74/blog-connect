f23229d5f9a51bab0c0166986ffefb5efca0997b
BabaYaGa74 <beeplovenepal74@gmail.com>
2023-11-21 11:02:15 +0545

    Update model

--- backend/models/VoteModel.js
+++ backend/models/VoteModel.js
@@ -3,8 +3,10 @@ const db = require("../config/dbConfig");
 const votemodel = {
   vote: (postId, userId) => {
     return new Promise((resolve, reject) => {
-      const query = "INSERT INTO vote (postId, userId) VALUES (?, ?)";
-      const values = [postId, userId];
+      const query =
+        "INSERT INTO vote (postId, userId, voteType) VALUES (?, ?, ?)";
+      const voteType = "upvote";
+      const values = [postId, userId, voteType];
       db.query(query, values, (err, result) => {
         if (err) reject(err);
         else resolve(result);
@@ -12,35 +14,53 @@ const votemodel = {
     });
   },
 
-  unvote: (postId, userId) => {
+  downvote: (postId, userId) => {
     return new Promise((resolve, reject) => {
-      const query = "DELETE FROM vote WHERE userId = ? AND postId = ?";
-      const values = [userId, postId];
+      const query =
+        "INSERT INTO vote (postId, userId, voteType) VALUES (?, ?, ?)";
+      const voteType = "downvote";
+      const values = [postId, userId, voteType];
       db.query(query, values, (err, result) => {
-        if (err) {
-          db.rollback(() => {
-            reject(err);
-          });
-        } else {
-          db.commit((err) => {
-            if (err) {
-              db.rollback(() => {
-                reject(err);
-              });
-            } else {
-              resolve(result);
-            }
-          });
-        }
+        if (err) reject(err);
+        else resolve(result);
       });
     });
   },
+
   getVoteCount: (postId) => {
     return new Promise((resolve, reject) => {
-      const qry = "SELECT COUNT(*) AS voteCount FROM vote WHERE postId = ?";
-      db.query(qry, [postId], (err, result) => {
+      let totalVotes = 0;
+      let upvotes = 0;
+      let downvotes = 0;
+      let queryCount = 0;
+
+      function queryChecker() {
+        if (queryCount == 2) {
+          totalVotes = upvotes - downvotes;
+          resolve(totalVotes);
+        }
+      }
+
+      const upvoteQry =
+        "SELECT COUNT(*) AS voteCount FROM vote WHERE postId = ? AND voteType = ?";
+      db.query(upvoteQry, [postId, "upvote"], (err, result) => {
         if (err) reject(err);
-        else resolve(result[0].voteCount);
+        else {
+          upvotes = result[0].voteCount;
+          queryCount++;
+          queryChecker();
+        }
+      });
+
+      const downQry =
+        "SELECT COUNT(*) AS voteCount FROM vote WHERE postId = ? AND voteType = ?";
+      db.query(downQry, [postId, "downvote"], (err, result) => {
+        if (err) reject(err);
+        else {
+          downvotes = result[0].voteCount;
+          queryCount++;
+          queryChecker();
+        }
       });
     });
   },
